ec076c9cbe727d9b2df5dfa79789eb0dd825e84b
KanhaKanha123 <kanaiyakamboj23@gmail.com>
2023-07-12 23:54:08 +0200

    typo error fixes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import ErrorBoundary from '../error-boundry/error-boundary';
+import ErrorBoundary from '../error-boundary/error-boundary';
 import { Home } from '../pages';
 import { BlogDetailsPage } from '../pages';
 export const router = createBrowserRouter([
